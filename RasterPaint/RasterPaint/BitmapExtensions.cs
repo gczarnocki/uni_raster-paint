887c454f471851eed21ec234f65b33b76a44ce4d
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-10-10 16:03:06 +0200

    Add the logic to the application and SizeChanged event.

---
+++ RasterPaint/RasterPaint/BitmapExtensions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace RasterPaint
+{
+    public static class BitmapExtensions
+    {
+        public static void SetPixel(this WriteableBitmap wb, int x, int y, Color c)
+        {
+            if (y > wb.PixelHeight - 1 || x > wb.PixelWidth - 1)
+            {
+                return;
+            }
+
+            if (y < 0 || x < 0)
+            {
+                return;
+            }
+
+            if (!wb.Format.Equals(PixelFormats.Bgra32))
+            {
+                return;
+            }
+
+            wb.Lock();
+            IntPtr buff = wb.BackBuffer;
+            int stride = wb.BackBufferStride;
+
+            unsafe
+            {
+                byte* pbuff = (byte*)buff.ToPointer();
+                int loc = y * stride + x * 4;
+                pbuff[loc] = c.B;
+                pbuff[loc + 1] = c.G;
+                pbuff[loc + 2] = c.R;
+                pbuff[loc + 3] = c.A;
+            }
+
+            wb.AddDirtyRect(new Int32Rect(x, y, 1, 1));
+            wb.Unlock();
+        }
+
+        public static Color GetPixel(this WriteableBitmap wbm, int x, int y)
+        {
+            if ((y > wbm.PixelHeight - 1 || x > wbm.PixelWidth - 1) || (y < 0 || x < 0))
+            {
+                return Color.FromArgb(0, 0, 0, 0);
+            }
+
+            if (!wbm.Format.Equals(PixelFormats.Bgra32))
+            {
+                return Color.FromArgb(0, 0, 0, 0);
+            }
+
+            IntPtr buff = wbm.BackBuffer;
+            int stride = wbm.BackBufferStride;
+            Color c;
+
+            unsafe
+            {
+                byte* pbuff = (byte*)buff.ToPointer();
+                int loc = y * stride + x * 4;
+                c = Color.FromArgb(pbuff[loc + 3], pbuff[loc + 2], pbuff[loc + 1], pbuff[loc]);
+            }
+
+            return c;
+        }
+
+        public static IEnumerable<Point> GetPoints(int x0, int y0, int x1, int y1)
+        {
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                int t;
+                t = x0; // swap x0 and y0
+                x0 = y0;
+                y0 = t;
+                t = x1; // swap x1 and y1
+                x1 = y1;
+                y1 = t;
+            }
+            if (x0 > x1)
+            {
+                int t;
+                t = x0; // swap x0 and x1
+                x0 = x1;
+                x1 = t;
+                t = y0; // swap y0 and y1
+                y0 = y1;
+                y1 = t;
+            }
+            int dx = x1 - x0;
+            int dy = Math.Abs(y1 - y0);
+            int error = dx / 2;
+            int ystep = (y0 < y1) ? 1 : -1;
+            int y = y0;
+            for (int x = x0; x <= x1; x++)
+            {
+                yield return new Point((steep ? y : x), (steep ? x : y));
+                error = error - dy;
+                if (error < 0)
+                {
+                    y += ystep;
+                    error += dx;
+                }
+            }
+            yield break;
+        }
+
+        public static void DrawLine(WriteableBitmap wb, Point startPoint, Point endPoint, Color c)
+        {
+            IEnumerable<Point> points = GetPoints((int)startPoint.X, (int)startPoint.Y, (int)endPoint.X, (int)endPoint.Y);
+
+            foreach (Point p in points)
+            {
+                SetPixel(wb, (int)p.X, (int)p.Y, c);
+            }
+        }
+
+        public static void Swap(int first, int second)
+        {
+            int t = first;
+            first = second;
+            second = t;
+        }
+    }
+}
--- RasterPaint/RasterPaint/MainWindow.xaml.cs
+++ RasterPaint/RasterPaint/MainWindow.xaml.cs
@@ -1,11 +1,18 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Shapes;
-using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-using System.Collections.Generic;
-using System;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace RasterPaint
 {
@@ -14,125 +21,224 @@ namespace RasterPaint
     /// </summary>
     public partial class MainWindow : Window
     {
+        WriteableBitmap wb;
+
+        bool removalMode;
+
         Point startPoint;
         Point endPoint;
         List<Point> pointsList;
-        List<Tuple<Point, Point>> linesList;
+        List<MyLine> linesList;
+        List<MyObject> objectsList;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            // wb = new WriteableBitmap((int)imageGrid.Width, (int)imageGrid.Height, 300, 300, PixelFormats.Bgra32, null); // size derived from myImage;
+            // myImage.Source = wb;
+
             pointsList = new List<Point>();
-            linesList = new List<Tuple<Point, Point>>();
+            linesList = new List<MyLine>();
+            objectsList = new List<MyObject>();
+
+            removalMode = false;
         }
 
-        private void DrawMyLine(Point startPoint, Point endPoint)
+        private void DrawGrid(object sender, RoutedEventArgs e)
         {
-            IEnumerable<Point> pointsCollection = GetPointsOnLine((int)startPoint.X, (int)startPoint.Y, (int)endPoint.X, (int)endPoint.Y);
-            
-            foreach(Point point in pointsCollection)
+            for(int i = 0; i < wb.PixelHeight * 2; i += 10)
             {
-                SetPixel(point, 1);
-            }          
-        }
-
-        public static IEnumerable<Point> GetPointsOnLine(int x0, int y0, int x1, int y1)
-        {
-            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-            if (steep)
-            {
-                int t;
-                t = x0; // swap x0 and y0
-                x0 = y0;
-                y0 = t;
-                t = x1; // swap x1 and y1
-                x1 = y1;
-                y1 = t;
-            }
-            if (x0 > x1)
-            {
-                int t;
-                t = x0; // swap x0 and x1
-                x0 = x1;
-                x1 = t;
-                t = y0; // swap y0 and y1
-                y0 = y1;
-                y1 = t;
-            }
-            int dx = x1 - x0;
-            int dy = Math.Abs(y1 - y0);
-            int error = dx / 2;
-            int ystep = (y0 < y1) ? 1 : -1;
-            int y = y0;
-            for (int x = x0; x <= x1; x++)
-            {
-                yield return new Point((steep ? y : x), (steep ? x : y));
-                error = error - dy;
-                if (error < 0)
+                for(int j = 0; j < wb.PixelWidth * 2; j += 10)
                 {
-                    y += ystep;
-                    error += dx;
+                    BitmapExtensions.SetPixel(wb, j, i, Colors.Red);
                 }
             }
-            yield break;
         }
 
-        private void SetPixel(Point p, int thickness)
+        private void myImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if(thickness >= 1)
+            if(removalMode)
             {
-                Ellipse myEllipse = new Ellipse
-                {
-                    Stroke = Brushes.Black,
-                    Width = Height = 1
-                };
+                Point p = e.GetPosition(myImage);
+                MyObject mo = new MyObject(null);
 
-                canvasObject.Children.Add(myEllipse);
-                Canvas.SetLeft(myEllipse, p.X);
-                Canvas.SetTop(myEllipse, p.Y);
+                foreach(var item in objectsList)
+                {
+                    if(item.objectBoundary.Contains(p))
+                    {
+                        mo = item;
+                        MessageBox.Show("Hit!");
+                        break;
+                    }
+                }
+
+                foreach(var item in mo.linesList)
+                {
+                    BitmapExtensions.DrawLine(wb, item.StartPoint, item.EndPoint, Colors.Red);
+                }
+            }
+            else
+            {
+                startPoint = e.GetPosition(myImage);
+                startPoint = SnapPoint(startPoint);
+                pointsList.Add(startPoint);
             }
         }
 
-        public static double GetDistanceBetweenPoints(Point p, Point q)
+        private void myImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            double a = p.X - q.X;
-            double b = p.Y - q.Y;
-            double distance = Math.Sqrt(a * a + b * b);
-            return distance;
-        }
-
-        private void canvasObject_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            endPoint = e.GetPosition(canvasObject);
-            endPoint = SnapPoint(endPoint);
-            pointsList.Add(endPoint);
-            linesList.Add(new Tuple<Point, Point>(startPoint, endPoint));
-
-            if (startPoint != endPoint)
+            if(!removalMode)
             {
-                DrawMyLine(startPoint, endPoint);
+                endPoint = e.GetPosition(myImage);
+                endPoint = SnapPoint(endPoint);
+                pointsList.Add(endPoint);
+
+                MyLine ml = new MyLine(startPoint, endPoint);
+                linesList.Add(ml);
+
+                MyObject mo = FindObjectFromPoint(startPoint, endPoint);
+
+                if (mo != null)
+                {
+                    mo.linesList.Add(ml);
+                }
+                else
+                {
+                    objectsList.Add(new MyObject(ml));
+                }
+
+                BitmapExtensions.DrawLine(wb, startPoint, endPoint, Colors.Black);
             }
         }
 
-        private void canvasObject_MouseDown(object sender, MouseButtonEventArgs e)
+        private void AddToList(Point p)
         {
-            startPoint = e.GetPosition(canvasObject);
-            startPoint = SnapPoint(startPoint);
-
-            pointsList.Add(startPoint);
+            if(!pointsList.Contains(p))
+            {
+                pointsList.Add(p);
+            }
         }
 
-        private Point SnapPoint(Point point)
+        private void ClearPoints()
         {
-            foreach(Point p in pointsList)
+            startPoint = new Point(0, 0);
+            endPoint = new Point(0, 0);
+        }
+
+        private Point SnapPoint(Point p)
+        {
+            foreach(Point point in pointsList)
             {
-                if(GetDistanceBetweenPoints(p, point) <= 15.0F)
+                if(DistanceBetweenPoints(p, point) <= 5.0F && !p.Equals(point))
                 {
-                    return p;
+                    return point;
                 }
             }
 
-            return point;
+            return p;
+        }
+
+        private double DistanceBetweenPoints(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+
+        private void removeObjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            removalMode = !removalMode;
+            removeObjectButton.Background = removalMode ? Brushes.LightGreen : Brushes.LightGray;
+        }
+
+        private MyObject FindObjectFromPoint(Point p_1, Point p_2)
+        {
+            foreach(var item in objectsList)
+            {
+                foreach(var line in item.linesList)
+                {
+                    if(p_1.Equals(line.StartPoint) || p_1.Equals(line.EndPoint) || p_2.Equals(line.StartPoint) || p_2.Equals(line.EndPoint))
+                    {
+                        return item;
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private void MergeObjects()
+        {
+            // method used when we merge objects by making a line which connects them;
+        }
+
+        private void imageGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            wb = new WriteableBitmap((int)e.NewSize.Width, (int)e.NewSize.Height, 300, 300, PixelFormats.Bgra32, null);
+            myImage.Source = wb;
+        }
+    }
+
+    class Boundary
+    {
+        double x_min = int.MaxValue;
+        double x_max = int.MinValue;
+        double y_min = int.MaxValue;
+        double y_max = int.MinValue;
+
+        public void UpdateBoundary(double x, double y)
+        {
+            if (x <= x_min) x_min = x;
+            if (x >= x_max) x_max = x;
+            if (y <= y_min) y_min = y;
+            if (y >= y_max) y_max = y;
+        }
+
+        public bool Contains(Point p)
+        {
+            if(p.X <= x_max && p.X >= x_min)
+            {
+                if (p.Y <= y_max && p.Y >= y_min)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    class MyLine
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public MyLine(Point startPoint, Point endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+    }
+
+    class MyObject
+    {
+        public List<MyLine> linesList;
+        public Boundary objectBoundary;
+
+        public MyObject(MyLine ml)
+        {
+            linesList = new List<MyLine>();
+            objectBoundary = new Boundary();
+            AddLine(ml);
+        }
+
+        public void AddLine(MyLine ml)
+        {
+            if(ml != null)
+            {
+                linesList.Add(ml);
+                objectBoundary.UpdateBoundary(ml.StartPoint.X, ml.StartPoint.Y);
+                objectBoundary.UpdateBoundary(ml.EndPoint.X, ml.EndPoint.Y);
+            }
         }
     }
 }
