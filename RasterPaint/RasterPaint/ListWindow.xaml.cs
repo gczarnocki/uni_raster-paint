a19b07dca54bae405e9f1cc4528eea0d78d6e781
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-10-18 13:04:54 +0200

    Add list of objects as a window and two converters to display information.

---
+++ RasterPaint/RasterPaint/ListWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RasterPaint
+{
+    /// <summary>
+    /// Interaction logic for ListWindow.xaml
+    /// </summary>
+    public partial class ListWindow : Window
+    {
+        public ListWindow(List<MyObject> moList)
+        {
+            InitializeComponent();
+            Objects.ItemsSource = new ObservableCollection<MyObject>(moList);
+        }
+    }
+}
---
+++ RasterPaint/RasterPaint/ObjectToWidthConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RasterPaint
+{
+    class ObjectToWidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var myObject = value as MyObject;
+
+            if (myObject != null)
+            {
+                if (myObject.Width > 0)
+                {
+                    return myObject.Width;
+                }
+                
+                return "(default)";
+            }
+
+            return "Width unknown!";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
