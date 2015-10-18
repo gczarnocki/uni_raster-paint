474917b8d7e3d48b38f4e14fc30ad6efde2d4107
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-10-18 13:44:39 +0200

    Add ObjectToColor converter and different display of object's color

---
+++ RasterPaint/RasterPaint/ObjectToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace RasterPaint
+{
+    class ObjectToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (value as MyObject).Color;
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
