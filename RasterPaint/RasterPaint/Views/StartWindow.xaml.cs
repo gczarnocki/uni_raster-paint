89682ffd4bf7ca873500e4d2057bf3674a91d6cb
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-11-10 22:19:02 +0100

    Move all styles from XAML files to App.xaml, add StartWindow

---
+++ RasterPaint/RasterPaint/Views/StartWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RasterPaint.Views;
+
+namespace RasterPaint
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mWnd = new MainWindow();
+            mWnd.Show();
+
+            // this.Close();
+        }
+
+        private void ColorReduction_Click(object sender, RoutedEventArgs e)
+        {
+            ColorReductionWindow crWnd = new ColorReductionWindow();
+            crWnd.Show();
+
+            // this.Close();
+        }
+    }
+}
