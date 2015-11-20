82d65f03a1afa2f54c56ef9e9f9404501d8825b7
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-11-20 10:53:38 +0100

    Add tests, one converter and fix images in help window.

---
+++ RasterPaint/RasterPaint/Converters/MultiValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace RasterPaint.Converters
+{
+    class MultiValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            //int v1 = (int)values[0];
+            //int v2 = (int)values[1];
+            //int v3 = (int)values[2];
+
+            //var result = v1 * v2 * v3;
+
+            //// Trace.WriteLine(result);
+            //return result.ToString();
+
+            return 0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- RasterPaint/RasterPaintTests/ColorReductionTests.cs
+++ RasterPaint/RasterPaintTests/ColorReductionTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,10 +12,107 @@ namespace RasterPaintTests
     [TestClass()]
     public class ColorReductionTests
     {
+        /// <summary>
+        /// Test for value = 0;
+        /// </summary>
         [TestMethod()]
-        public void UniformQuantizationTest1()
+        public void CheckIfZeroReturnsZero() // początek przedziału;
         {
-            
+            byte pixelValue = 141;
+            var value = ColorReduction.ReducePixelUqForTests(0, pixelValue);
+
+            Assert.AreEqual(pixelValue, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing0Within2Intervals()
+        {
+            byte pixelValue = 0;
+            byte expected = 64;
+            var value = ColorReduction.ReducePixelUqForTests(2, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing0Within4Intervals()
+        {
+            byte pixelValue = 0;
+            byte expected = 32;
+            var value = ColorReduction.ReducePixelUqForTests(4, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing255Within2Intervals()
+        {
+            byte pixelValue = 255;
+            byte expected = 192;
+            var value = ColorReduction.ReducePixelUqForTests(2, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing255Within4Intervals()
+        {
+            byte pixelValue = 255;
+            byte expected = 224;
+            var value = ColorReduction.ReducePixelUqForTests(4, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing0Within6Intervals()
+        {
+            byte pixelValue = 0;
+            byte expected = 21;
+            var value = ColorReduction.ReducePixelUqForTests(6, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducing255Within6Intervals()
+        {
+            byte pixelValue = 255;
+            byte expected = 232;
+            var value = ColorReduction.ReducePixelUqForTests(6, pixelValue);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducingIntoSecondIntervalWithin3Intervals()
+        {
+            byte pixelValue = 127; // 256 / 3 = ~ 85; within second interval;
+            byte expected = 85 + (85 - 1)/2;
+
+            var value = ColorReduction.ReducePixelUqForTests(3, pixelValue);
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod()]
+        public void CheckReducingIntoThirdIntervalWithin3Intervals()
+        {
+            byte pixelValue = 253; // 256 / 3 = ~ 85; 255 - (85 * 2) = 85; 85 / 2 = ~ 42;
+            byte expected = 85 + 85 + 42;
+
+            var value = ColorReduction.ReducePixelUqForTests(3, pixelValue);
+            Assert.AreEqual(expected, value);
+        }
+        
+        [TestMethod()]
+        public void CheckReducingIntoFourthIntervalWithin11Intervals()
+        {
+            byte fraction = (byte)Math.Floor((double)255/11);
+            byte pixelValue = (byte) (3*fraction + 1);
+            byte expected = (byte)Math.Floor((double)3*fraction + fraction/2);
+
+            var value = ColorReduction.ReducePixelUqForTests(11, pixelValue);
+            Assert.AreEqual(expected, value);
         }
     }
 }
