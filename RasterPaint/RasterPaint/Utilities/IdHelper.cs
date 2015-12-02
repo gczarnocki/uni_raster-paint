4c94640f8122e0c74c30175737aa27ad2bdbf95b
Grzegorz Czarnocki <g.czarnocki1@gmail.com>
2015-12-02 19:04:11 +0100

    Fix the Octree Quantization Algorithm and refactor Node class

---
+++ RasterPaint/RasterPaint/Utilities/IdHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasterPaint.Utilities
+{
+    public static class IdHelper
+    {
+        public static uint Identifier { get; set; } = 1;
+
+        public static uint NewId()
+        {
+            return Identifier++;
+        }
+    }
+}
